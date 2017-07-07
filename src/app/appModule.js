f5e108868c02b20057789699021dec89234a269b
Leszek Kłoczko <132784@student.uwm.edu.pl>
2017-07-07 12:49:31 +0200

    font

--- src/app/appModule.js
+++ src/app/appModule.js
@@ -9,7 +9,6 @@ import '../../node_modules/angular-material/angular-material.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-
 import './style.css';
 
 import CommonModule from './components/common/commonModule';
